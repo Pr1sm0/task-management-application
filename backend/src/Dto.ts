ec38aaca986b94d94840ac44f2bc8f61aba4e948
Vasyl Sava <Vasyard@gmail.com>
2021-02-03 16:34:55 +0200

    Add ITask interface, add task model and schema for the db

--- backend/src/Dto.ts
+++ backend/src/Dto.ts
@@ -1,6 +1,7 @@
-import { Document } from 'mongoose';
+import { Document, Types } from 'mongoose';
 
 export namespace DTO {
+  export type ID = Types.ObjectId;
   export interface IUser {
     name: string;
     photoUrl: string;
@@ -11,5 +12,27 @@ export namespace DTO {
   export interface IEmail {
     email: string;
   }
+
+  export interface ITask {
+    title: string;
+    description: string;
+    priority: taskPriorities;
+    statusId: ID;
+    assigneeId: ID;
+    createdAt: Date;
+    updatedAt: Date;
+    boardId: ID;
+  }
+
   export interface IUserDoc extends IUser, Document {}
+  export interface ITaskDoc extends ITask, Document {}
+
+  export enum taskPriorities {
+    HIGHEST = 'HIGHEST',
+    HIGH = 'HIGH',
+    MEDIUM = 'MEDIUM',
+    LOW = 'LOW',
+    LOWEST = 'LOWEST',
+  }
 }
+
